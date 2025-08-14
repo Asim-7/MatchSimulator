90fe8658e0001d0cad5d74676c1a93aa2a56dfed
asim.shehzad <asim.shehzad@tomtom.com>
2025-08-14 12:49:52 +0200

    Dummy data improved

---
+++ app/src/main/java/com/miniclip/matchsimulator/data/model/TeamInfo.kt
@@ -0,0 +1,7 @@
+package com.miniclip.matchsimulator.data.model
+
+data class TeamInfo(
+    val name: String,
+    val logo: Int,
+    val strength: Int
+)
--- app/src/main/java/com/miniclip/matchsimulator/data/remote/DummyData.kt
+++ app/src/main/java/com/miniclip/matchsimulator/data/remote/DummyData.kt
@@ -2,86 +2,114 @@ package com.miniclip.matchsimulator.data.remote
 
 import com.miniclip.matchsimulator.data.model.MatchEntity
 import com.miniclip.matchsimulator.R
+import com.miniclip.matchsimulator.data.model.TeamInfo
 
 object DummyData {
+
+    private const val FIRST_TIME = "18:30 CET"
+    private const val SECOND_TIME = "21:00 CET"
+
+    val teamInfo = listOf(
+        TeamInfo(
+            name = "Ajax",
+            logo = R.drawable.ajax,
+            strength = 88
+        ),
+        TeamInfo(
+            name = "PSV",
+            logo = R.drawable.psv,
+            strength = 84
+        ),
+        TeamInfo(
+            name = "Feyenoord",
+            logo = R.drawable.feyenoord,
+            strength = 78
+        ),
+        TeamInfo(
+            name = "Utrecht",
+            logo = R.drawable.utrecht,
+            strength = 72
+        )
+    )
+
     val matches = listOf(
         MatchEntity(
             matchDay = 1,
-            homeTeam = "Ajax",
-            homeTeamStrength = 85,
-            awayTeam = "PSV",
-            awayTeamStrength = 82,
+            homeTeam = teamInfo[0].name,
+            homeTeamStrength = teamInfo[0].strength,
+            awayTeam = teamInfo[1].name,
+            awayTeamStrength = teamInfo[1].strength,
             homeScore = null,
             awayScore = null,
             matchDate = "10/10/2025",
-            matchTime = "18:30 CET",
-            homeTeamLogo = R.drawable.ajax,
-            awayTeamLogo = R.drawable.psv
+            matchTime = FIRST_TIME,
+            homeTeamLogo = teamInfo[0].logo,
+            awayTeamLogo = teamInfo[1].logo
         ),
         MatchEntity(
             matchDay = 1,
-            homeTeam = "Feyenoord",
-            homeTeamStrength = 80,
-            awayTeam = "Utrecht",
-            awayTeamStrength = 75,
+            homeTeam = teamInfo[2].name,
+            homeTeamStrength = teamInfo[2].strength,
+            awayTeam = teamInfo[3].name,
+            awayTeamStrength = teamInfo[3].strength,
             homeScore = null,
             awayScore = null,
             matchDate = "10/10/2025",
-            matchTime = "21:00 CET",
-            homeTeamLogo = R.drawable.feyenoord,
-            awayTeamLogo = R.drawable.utrecht
+            matchTime = SECOND_TIME,
+            homeTeamLogo = teamInfo[2].logo,
+            awayTeamLogo = teamInfo[3].logo
         ),
         MatchEntity(
             matchDay = 2,
-            homeTeam = "Utrecht",
-            homeTeamStrength = 75,
-            awayTeam = "PSV",
-            awayTeamStrength = 82,
+            homeTeam = teamInfo[3].name,
+            homeTeamStrength = teamInfo[3].strength,
+            awayTeam = teamInfo[1].name,
+            awayTeamStrength = teamInfo[1].strength,
             homeScore = null,
             awayScore = null,
             matchDate = "12/10/2025",
-            matchTime = "18:30 CET",
-            homeTeamLogo = R.drawable.utrecht,
-            awayTeamLogo = R.drawable.psv
+            matchTime = FIRST_TIME,
+            homeTeamLogo = teamInfo[3].logo,
+            awayTeamLogo = teamInfo[1].logo
         ),
         MatchEntity(
             matchDay = 2,
-            homeTeam = "Feyenoord",
-            homeTeamStrength = 80,
-            awayTeam = "Ajax",
-            awayTeamStrength = 85,
+            homeTeam = teamInfo[2].name,
+            homeTeamStrength = teamInfo[2].strength,
+            awayTeam = teamInfo[0].name,
+            awayTeamStrength = teamInfo[0].strength,
             homeScore = null,
             awayScore = null,
             matchDate = "12/10/2025",
-            matchTime = "21:00 CET",
-            homeTeamLogo = R.drawable.feyenoord,
-            awayTeamLogo = R.drawable.ajax
+            matchTime = SECOND_TIME,
+            homeTeamLogo = teamInfo[2].logo,
+            awayTeamLogo = teamInfo[0].logo
         ),
         MatchEntity(
             matchDay = 3,
-            homeTeam = "Ajax",
-            homeTeamStrength = 85,
-            awayTeam = "Utrecht",
-            awayTeamStrength = 75,
+            homeTeam = teamInfo[0].name,
+            homeTeamStrength = teamInfo[0].strength,
+            awayTeam = teamInfo[3].name,
+            awayTeamStrength = teamInfo[3].strength,
             homeScore = null,
             awayScore = null,
             matchDate = "14/10/2025",
-            matchTime = "18:30 CET",
-            homeTeamLogo = R.drawable.ajax,
-            awayTeamLogo = R.drawable.utrecht
+            matchTime = FIRST_TIME,
+            homeTeamLogo = teamInfo[0].logo,
+            awayTeamLogo = teamInfo[3].logo
         ),
         MatchEntity(
             matchDay = 3,
-            homeTeam = "PSV",
-            homeTeamStrength = 82,
-            awayTeam = "Feyenoord",
-            awayTeamStrength = 80,
+            homeTeam = teamInfo[1].name,
+            homeTeamStrength = teamInfo[1].strength,
+            awayTeam = teamInfo[2].name,
+            awayTeamStrength = teamInfo[2].strength,
             homeScore = null,
             awayScore = null,
             matchDate = "14/10/2025",
-            matchTime = "21:00 CET",
-            homeTeamLogo = R.drawable.psv,
-            awayTeamLogo = R.drawable.feyenoord
+            matchTime = SECOND_TIME,
+            homeTeamLogo = teamInfo[1].logo,
+            awayTeamLogo = teamInfo[2].logo
         )
     )
 }
