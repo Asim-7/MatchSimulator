15fbac87a749e4228d847327927c13b2c4db5e04
asim.shehzad <asim.shehzad@tomtom.com>
2025-08-15 15:09:10 +0200

    Minor improvement done

--- app/src/main/java/com/miniclip/matchsimulator/ui/main/components/CommonTopBar.kt
+++ app/src/main/java/com/miniclip/matchsimulator/ui/main/components/CommonTopBar.kt
@@ -7,7 +7,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,7 +27,7 @@ fun CustomTopBar(
     onResetClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var selectedTab by remember { mutableIntStateOf(0) }
+    var selectedTab by rememberSaveable { mutableIntStateOf(0) }
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
