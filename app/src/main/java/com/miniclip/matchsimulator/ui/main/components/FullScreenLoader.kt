81a3bdb5c3ff19a8f899013ef86bcfd6f594ecf4
asim.shehzad <asim.shehzad@tomtom.com>
2025-08-17 11:23:28 +0200

    Animation smoothness done

--- app/src/main/java/com/miniclip/matchsimulator/ui/main/components/FullScreenLoader.kt
+++ app/src/main/java/com/miniclip/matchsimulator/ui/main/components/FullScreenLoader.kt
@@ -72,6 +72,7 @@ fun FullScreenLoader(
         ) {
             LottieAnimation(
                 composition = composition,
+                iterations = 2,
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(Dimens.padding_32)
